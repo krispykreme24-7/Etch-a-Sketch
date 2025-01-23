0f90506aaa0ada2b84be77be02b310f729e4db6b
krispykreme24-7 <sebastianinmalta@aim.com>
2025-01-23 14:45:31 -0500

    Added html file, JS file, and CSS file

---
+++ index.js
@@ -0,0 +1,9 @@
+const divGrid1 = document.createElement('div');
+const container = document.querySelector(".grid-container")
+divGrid1.textContent = "Test";
+container.appendChild(divGrid1);
+
+
+for (let i = 0; i<16; i++){
+    const divGrid = document.createElement('div');
+}
