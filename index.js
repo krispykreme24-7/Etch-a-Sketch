134e418a927cd7e61cf12df4fc3468fdad3ebf15
krispykreme24-7 <sebastianinmalta@aim.com>
2025-01-24 12:39:52 -0500

    Created the base grid that reacts to hover

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const divGrid1 = document.createElement('div');
 const container = document.querySelector(".grid-container")
-divGrid1.textContent = "Test";
-container.appendChild(divGrid1);
 
 
-for (let i = 0; i<16; i++){
-    const divGrid = document.createElement('div');
+const gridSize = 16;
+
+for (let i = 1; i <= gridSize*gridSize; i++){
+    const gridItem = document.createElement('div');
+    gridItem.classList = 'gridItem';
+    gridItem.textContent = [i];
+    container.appendChild(gridItem);
 }
+
+document.addEventListener("mouseover", (event) => {
+    if (!event.target.style.backgroundColor) { 
+      // Apply the background color only if it hasn’t been applied yet
+      event.target.style.backgroundColor = "green";
+    }
+  });
+  
+
+
+/* onmouseover = (event) => {}; */
